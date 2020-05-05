b408a432a290edef66ed33d91bf06c31e593f18b
Alex Carney <alcarneyme@gmail.com>
2020-05-05 17:58:59 +0100

    sudo apt install..

--- scripts/setup-tinygo.sh
+++ scripts/setup-tinygo.sh
@@ -3,7 +3,7 @@
 # Script to bootstrap tinygo in Github Actions
 
 url="https://github.com/tinygo-org/tinygo/releases/download/v${VERSION}/tinygo_${VERSION}_amd64.deb"
-curl -s $url --output "tinygo_${VERSION}.deb"
+curl $url --output "tinygo_${VERSION}.deb"
 
-apt install "tinygo_${VERSION}.deb"
+sudo apt install "tinygo_${VERSION}.deb"
 tinygo
