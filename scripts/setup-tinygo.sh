58ceea9a3e8f5e80497011249d581d71af5d3b2e
Alex Carney <alcarneyme@gmail.com>
2020-05-05 18:00:48 +0100

    Need to explicitly pass as a path?

--- scripts/setup-tinygo.sh
+++ scripts/setup-tinygo.sh
@@ -5,5 +5,5 @@
 url="https://github.com/tinygo-org/tinygo/releases/download/v${VERSION}/tinygo_${VERSION}_amd64.deb"
 curl $url --output "tinygo_${VERSION}.deb"
 
-sudo apt install "tinygo_${VERSION}.deb"
+sudo apt install "./tinygo_${VERSION}.deb"
 tinygo
