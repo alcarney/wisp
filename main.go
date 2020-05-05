2430cd185a5ccdb28c7b1087deaec7c405b4502f
Alex Carney <alcarneyme@gmail.com>
2020-05-05 17:18:11 +0100

    Basic string passing

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-// go:export main
+var buf [1024]byte
+
+// Functions to be implemented by the JS wrapper
+func log(message string)
+
+//go:export echo
+func echo(message string) {
+	log(message)
+}
+
+//go:export getBuffer
+func getBuffer() *byte {
+	return &buf[0]
+}
+
 func main() {
-	println("Hello world!")
+
 }
