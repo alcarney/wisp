a7270576ad91a5be136a6163877d81a7b81cd8ab
Alex Carney <alcarneyme@gmail.com>
2020-05-06 16:57:37 +0100

    Proof of concept implementation

--- main.go
+++ main.go
@@ -9,16 +9,48 @@ var buf [1024]byte
 
 // Program represents the series of instructions to execute
 type Program struct {
-	instructions string
-	pointer      int
+	instructions string      // The sequence of instructions to execute
+	pointer      int         // Index to the current instruction
+	brackets     map[int]int // Map to keep track of control points
 }
 
 func (p *Program) nextInstruction(tape Tape) rune {
+
+	// Stop when we reach the end.
 	if p.pointer > len(p.instructions)-1 {
 		return -1
 	}
 
 	ins := p.instructions[p.pointer]
+	val := tape.getValue()
+
+	if ins == '[' {
+
+		// Jump past the matching ']' if the value in the current cell is 0
+		if val == 0 {
+			p.pointer = p.brackets[p.pointer] + 1
+			return p.nextInstruction(tape)
+		}
+
+		// Otherwise return the next instruction
+		p.pointer++
+		return p.nextInstruction(tape)
+	}
+
+	if ins == ']' {
+
+		// Jump back to the matching '[' if the value in the current cell is NOT 0
+		if val != 0 {
+			p.pointer = p.brackets[p.pointer]
+			return p.nextInstruction(tape)
+		}
+
+		// Otherwise return the next instruction
+		p.pointer++
+		return p.nextInstruction(tape)
+	}
+
+	// Advance the instruction pointer for next time.
 	p.pointer++
 
 	return rune(ins)
@@ -60,20 +92,18 @@ func (t *Tape) getValue() int {
 func log(message string)
 func output(message string)
 
-//go:export echo
-func echo(message string) {
-	log(message)
-	output(parseInput(message))
-}
-
 //go:export runBf
 func runBf(input string) {
-	instructions := parseInput(input)
-	log(fmt.Sprintf("Executing program: '%s'", instructions))
+	program, err := parseInput(input)
 
-	program := Program{pointer: 0, instructions: instructions}
+	if err != nil {
+		log(fmt.Sprintf("Syntax Error: %s", err))
+		return
+	}
+
+	log(fmt.Sprintf("Executing program: '%s'", program.instructions))
+
 	tape := Tape{pointer: 0, cells: make(map[int]int)}
-
 	executeProgram(program, tape)
 }
 
@@ -82,31 +112,47 @@ func getBuffer() *byte {
 	return &buf[0]
 }
 
-func parseInput(input string) string {
-	program := ""
-	openBrackets := 0
+func parseInput(input string) (Program, error) {
+	brackets := []int{}
+	bracketMap := make(map[int]int)
+	instructions := ""
 
-	for _, char := range input {
+	for idx, char := range input {
+		var start int
 
 		if strings.ContainsRune("<>+-.,[]", char) {
 			if char == '[' {
-				openBrackets++
+				brackets = append(brackets, idx)
 			}
 
 			if char == ']' {
 
+				if len(brackets) == 0 {
+					return Program{}, fmt.Errorf("unmatched brackets")
+				}
+
+				// Pair the most recently pushed open bracket with the current index
+				start, brackets = brackets[len(brackets)-1], brackets[:len(brackets)-1]
+				bracketMap[start] = idx
+				bracketMap[idx] = start
 			}
 
-			program += string(char)
+			instructions += string(char)
 		}
 	}
 
-	return program
+	if len(brackets) != 0 {
+		return Program{}, fmt.Errorf("unmatched brackets")
+	}
+
+	program := Program{instructions: instructions, pointer: 0, brackets: bracketMap}
+	return program, nil
 }
 
 func executeProgram(program Program, tape Tape) {
 
 	ins := program.nextInstruction(tape)
+	log("Ins: '" + string(ins) + "'")
 
 	for ins > 0 {
 
@@ -132,13 +178,11 @@ func executeProgram(program Program, tape Tape) {
 			break
 
 		case ',':
-		case ']':
-		case '[':
 			log("Not yet implemented")
 			break
 
 		default:
-			panic("Invalid instruction")
+			panic("Invalid instruction: " + string(ins))
 		}
 
 		ins = program.nextInstruction(tape)
